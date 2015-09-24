5be6b33a117d48df5c253eeac3d809ef0bfd8ffa
mengcz13 <mengcz13@163.com>
2015-09-25 00:57:24 +0800

    solve for 3*3

--- posture_pw.cpp
+++ posture_pw.cpp
@@ -12,66 +12,58 @@ struct Point{
 	int y;
 };
 
-void search_pattern(int startx, int starty, int layer){
+void search_pattern(int orix, int oriy, int startx, int starty, int layer){
+	pattern_type[orix][oriy][layer]+=1;
 	pattern_area[startx][starty]=1;
 	std::queue<Point> avalpoints;
-	std::vector<Point> neighbors;
-	Point tempp;
-	tempp.x=startx-1;tempp.y=starty-1;neighbors.push_back(tempp);
-	tempp.x=startx;tempp.y=starty-1;neighbors.push_back(tempp);
-	tempp.x=startx+1;tempp.y=starty-1;neighbors.push_back(tempp);
-	tempp.x=startx-1;tempp.y=starty;neighbors.push_back(tempp);
-	tempp.x=startx+1;tempp.y=starty;neighbors.push_back(tempp);
-	tempp.x=startx-1;tempp.y=starty+1;neighbors.push_back(tempp);
-	tempp.x=startx;tempp.y=starty+1;neighbors.push_back(tempp);
-	tempp.x=startx+1;tempp.y=starty+1;neighbors.push_back(tempp);
-	for (int i=0;i<neighbors.size;++i){
-		if (neighbors[i].x<0||neighbors[i].x>=SIZE||neighbors[i].y<0||neighbors[i].y>=SIZE){
-			continue;
-		}
-		else if (pattern_area[neighbors[i].x][neighbors[i].y]==0){
-			Point t;
-			t.x=neighbors[i].x;t.y=neighbors[i].y;
-			avalpoints.push(t);
-		}
-		else{
-			Point vec_ori2nei;
-			vec_ori2nei.x=neighbors[i].x-startx;vec_ori2nei.y=neighbors[i].y-starty;
-			int tx=neighbors[i].x, ty=neighbors[i].y;
-			while(tx>=0&&tx<SIZE&&ty>=0&&ty<SIZE){
-				if (pattern_area[tx][ty]==0)
-					break;
-				tx+=vec_ori2nei.x;
-				ty+=vec_ori2nei.y;
-			}
-			if (tx>=0&&tx<SIZE&&ty>=0&&ty<SIZE){
-				Point temp;
-				temp.x=tx;temp.y=ty;
-				avalpoints.push(temp);
+	for (int i=0;i<SIZE;++i)
+		for (int j=0;j<SIZE;++j){
+			if (startx==i&&starty==j||pattern_area[i][j]==1)
+				continue;
+			else{
+				bool flag=true;
+				for (int ii=0;ii<SIZE;++ii)
+					for (int jj=0;jj<SIZE;++jj){
+						if (pattern_area[ii][jj]==0){
+							if (!(ii==i&&jj==j)&&(ii-startx)*(ii-i)<=0&&(jj-starty)*(jj-j)<=0&&(ii-startx)*(jj-j)==(jj-starty)*(ii-i))
+								flag=false;
+						}
+					}
+				if (flag){
+					Point temp;
+					temp.x=i;temp.y=j;
+					avalpoints.push(temp);
+				}
 			}
 		}
+	while (!avalpoints.empty()){
+		int nx=avalpoints.front().x;
+		int ny=avalpoints.front().y;
+		pattern_area[nx][ny]=1;
+		search_pattern(orix,oriy,nx,ny,layer+1);
+//		pattern_area[nx][ny]=0;
+		avalpoints.pop();
 	}
+	pattern_area[startx][starty]=0;
+	return;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+int main(){
+	for (int i=0;i<SIZE;++i)
+		for (int j=0;j<SIZE;++j){
+			pattern_area[i][j]=0;
+			for (int k=0;k<SIZE*SIZE+1;++k)
+				pattern_type[i][j][k]=0;
+		}
+	for (int i=0;i<SIZE;++i)
+		for (int j=0;j<SIZE;++j)
+			search_pattern(i,j,i,j,1);
+	for (int i=0;i<SIZE;++i)
+		for (int j=0;j<SIZE;++j){
+			printf("point(%d,%d)\n",i,j);
+			for (int k=1;k<SIZE*SIZE+1;++k)
+				printf("layer:%d	%d\n",k,pattern_type[i][j][k]);
+		}
+	return 0;
+}
 
